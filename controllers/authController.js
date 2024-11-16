23afccc049d8405aff40f57bd33bf4392e059d1f
last-hop <amansingh.28757@gmail.com>
2024-11-16 15:21:29 +0530

    testing

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ exports.loginUser = async (req, res) => {
     try {
         const user = await User.findOne({ email });
         if (user && await user.matchPassword(password)) {
-            res.json({ token: generateToken(user._id) });
+            res.json({ token: generateToken(user._id),"anything":"abcd"});
         } else {
             res.status(401).json({ message: 'Invalid email or password' });
         }
--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use('/quiz',  quizRoutes);
 
 
 // Start the server
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
